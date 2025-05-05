0b53cc09d95e5ebacc137b341baac9cd7c42b27e
Sotaro Ichii <sotta.san17@gmail.com>
2025-05-04 22:39:05 -0700

    Fix route conflict and use CourseDetail for /course/:id

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Semester from './pages/Semester.jsx';
 import Comments from './pages/Comments.jsx';
-import Course from './pages/Course.jsx';
+import CourseDetail from './pages/CourseDetail.jsx'; // ← 詳細ページとして採用
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />  // ✅ 検索フォームがあるHomeを表示
+        <Route path="/" element={<Home />} />
         <Route path="/semester/:id" element={<Semester />} />
         <Route path="/comments" element={<Comments />} />
-        <Route path="/course/:id" element={<Course />} />
+        <Route path="/course/:id" element={<CourseDetail />} /> {/* ← ここをCourseDetailで統一 */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
 
