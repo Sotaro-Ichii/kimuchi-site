3762e0998117ada79e7d8c92fd748393fca9589b
Sotaro Ichii <sotta.san17@gmail.com>
2025-05-05 20:39:07 -0700

    Implement Stripe checkout and hide course details until payment

---
+++ src/lib/stripe.js
@@ -0,0 +1,4 @@
+// src/lib/stripe.js
+import { loadStripe } from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe("pk_test_pk_live_51RLWqpE8746HD9eICX305FFGJ7xC8Cli628PbGnMtGWsaFUDYHatCeOAKxRpq3VxCIyh1WpBKAZ9p0TUt4vDDDO100zIAYRxLR");
