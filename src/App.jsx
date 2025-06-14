71eb5c01c25fdfbc69f41870ecb8787034be8974
Sotaro Ichii <sotta.san17@gmail.com>
2025-06-14 06:59:00 -0700

    updated lp

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Home from './pages/Home';
+import SemesterPage from './pages/SemesterPage';
+import CommentsPage from './pages/CommentsPage';
+import Legal from './pages/Legal';
+import Tokushoho from './pages/Tokushoho';
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Kimchi（キムチ）へようこそ</h1>
-      <p>Orange Coast Collegeの履修登録をサポートします。</p>
-      <ul>
-        <li><Link to="/semester/1">1学期に取るべき授業</Link></li>
-        <li><Link to="/semester/2">2学期に取るべき授業</Link></li>
-        <li><Link to="/semester/3">3学期に取るべき授業</Link></li>
-        <li><Link to="/semester/4">4学期に取るべき授業</Link></li>
-        <li><Link to="/comments">コメント機能</Link></li>
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        {/* ✅ 最初に表示されるLP */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* ✅ 本会員のみアクセスするホーム画面 */}
+        <Route path="/home" element={<Home />} />
+
+        {/* ✅ 学期別ページ */}
+        <Route path="/semester/:id" element={<SemesterPage />} />
+
+        {/* ✅ コメントページ */}
+        <Route path="/comments" element={<CommentsPage />} />
+
+        {/* ✅ 法律ページ */}
+        <Route path="/legal" element={<Legal />} />
+        <Route path="/tokushoho" element={<Tokushoho />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
 
 
