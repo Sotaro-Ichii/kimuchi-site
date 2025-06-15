4661a37c75a9b8afc201f46eca1288033a4c6844
Sotaro Ichii <sotta.san17@gmail.com>
2025-06-14 17:00:59 -0700

    Updated4

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage.jsx'; // ✅ 追加
 import Home from './pages/Home.jsx';
 import Semester from './pages/Semester.jsx';
 import Comments from './pages/Comments.jsx';
-import CourseDetail from './pages/CourseDetail.jsx'; // ← 詳細ページとして採用
+import CourseDetail from './pages/CourseDetail.jsx';
 import Legal from './pages/Legal.jsx';
 import Tokushoho from './pages/Tokushoho.jsx';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} /> {/* ✅ 修正：ここをLandingPageに */}
+        <Route path="/home" element={<Home />} />
         <Route path="/semester/:id" element={<Semester />} />
         <Route path="/comments" element={<Comments />} />
-        <Route path="/course/:id" element={<CourseDetail />} /> {/* ← ここをCourseDetailで統一 */}
+        <Route path="/course/:id" element={<CourseDetail />} />
         <Route path="/legal" element={<Legal />} />
         <Route path="/tokushoho" element={<Tokushoho />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
 
