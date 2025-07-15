47b926fb3e77d1ca259180cf33d1940c25b7d37f
Sotaro Ichii <sotta.san17@gmail.com>
2025-07-15 00:33:27 -0700

    fix bug

--- src/pages/DemoHome.jsx
+++ src/pages/DemoHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaUser, FaComment, FaArrowRight } from "react-icons/fa";
+import { FaUser, FaArrowRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const sampleCourses = [
